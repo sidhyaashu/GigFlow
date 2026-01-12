b1966d9e4e3e52702ae2ce397afbfccf2166f71f
Asutosh Sidhya <ASHUTOSHSIDHYA69@GMAIL.COM>
2026-01-13 00:02:26 +0530

    Update gigsApiSlice.ts

--- client/src/features/gigs/gigsApiSlice.ts
+++ client/src/features/gigs/gigsApiSlice.ts
@@ -4,25 +4,25 @@ export const gigsApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder: any) => ({
         getGigs: builder.query({
             query: (keyword: string) => ({
-                url: 'https://gigflow-backend-bj4g.onrender.com/api/gigs',
+                url: '/gigs',
                 params: { search: keyword },
             }),
             providesTags: ['Gig'],
         }),
         createGig: builder.mutation({
             query: (data: any) => ({
-                url: 'https://gigflow-backend-bj4g.onrender.com/api/gigs',
+                url: '/gigs',
                 method: 'POST',
                 body: data,
             }),
             invalidatesTags: ['Gig'],
         }),
         getMyGigs: builder.query({
-            query: () => 'https://gigflow-backend-bj4g.onrender.com/api/gigs/my-gigs',
+            query: () => '/gigs/my-gigs',
             providesTags: ['Gig'],
         }),
         getGigById: builder.query({
-            query: (id: string) => `https://gigflow-backend-bj4g.onrender.com/api/gigs/${id}`,
+            query: (id: string) => `/gigs/${id}`,
             providesTags: ['Gig'],
         }),
     }),
